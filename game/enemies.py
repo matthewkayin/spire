d4506d6c03a231ecb57cf1fe5db46adf8dc533bd
matthewkayin <matthewkayin@gmail.com>
2020-01-25 11:37:00 -0600

    Templatized the enemies
    Signed-off-by: matthewkayin <matthewkayin@gmail.com>

--- game/enemies.py
+++ game/enemies.py
@@ -6,19 +6,12 @@ class Enemy(entity.Entity):
     Class for a basic enemy
     """
 
-    def __init__(self, x, y):
-        super(Enemy, self).__init__("mummy", True)
-        self.x = x
-        self.y = y
+    def __init__(self, image, x, y, move_speed, starting_health):
+        super(Enemy, self).__init__(image, True)
+        self.x, self.y = x, y
 
-        self.FOLLOW_RANGE = 600
-        self.MOVE_SPEED = 1
+        self.MOVE_SPEED = move_speed
 
-        """
-        The attack windup occurs when the enemy is in melee range of the player (self.ATTACK_RANGE)
-        The windup hurtbox is based on the player location when the attack begins. After timer goes off,
-        if the player is still in the hurtbox, the player will be dealt self.POWER damage
-        """
         self.attacking = False
         self.attack_timer = 0
         self.attack_speed_percent = 1
@@ -28,38 +21,14 @@ class Enemy(entity.Entity):
         self.hurtbox = None
         self.POWER = 0.5
 
-        self.health = 10
+        self.health = starting_health
         self.interactions = []
 
     def update(self, dt, player_rect):
-        # If we finished an attack last turn, we want to reset these variables so the hit happens only once
-        if self.deal_damage:
-            self.hurtbox = None
-            self.deal_damage = False
+        self.do_ai(dt, player_rect)
 
-        # If we're in an attack windup...
-        if self.attacking:
-            self.attack_timer += (dt * self.attack_speed_percent)
-            # If the attack windup has finished, set the variables needed for us to deal the player damage in the update loop
-            if self.attack_timer >= self.ATTACK_SPEED:
-                self.deal_damage = True
-                self.attacking = False
-        else:
-            player_center = util.get_center(player_rect)
-            self_center = util.get_center((self.x, self.y, self.width, self.height))
-            player_distance = util.get_distance(self_center, player_center)
-            if self.collides(player_rect):
-                # begin an attack windup
-                self.attacking = True
-                self.hurtbox = player_rect
-                self.attack_timer = 0
-                self.vx, self.vy = (0, 0)
-            elif player_distance <= self.FOLLOW_RANGE:
-                # Vetorize movement relative to player
-                vector_to_player = ((player_center[0] - self_center[0]), (player_center[1] - self_center[1]))
-                self.vx, self.vy = util.scale_vector(vector_to_player, self.MOVE_SPEED)
-
-        self.attack_speed_percent = 1  # Reset attack speed each update in case it was affected by a slow
+        # reset attack speed each update so that when interactions fizzle out attack speed returns to normal
+        self.attack_speed_percent = 1
 
         self.image_append = ""
         for interaction in self.interactions:
@@ -68,11 +37,12 @@ class Enemy(entity.Entity):
 
         super(Enemy, self).update(dt)
 
-    def handle_spell_action(self, action, action_value):
-        if action == spells.Spell.DAMAGE:
-            self.health -= action_value
-        elif action == spells.Spell.FREEZE:
-            self.freeze_timer = action_value
+    def do_ai(self, dt, player_rect):
+        """
+        This is a to-be-implimented function for each enemy and it describes the specific AI behavior
+        for the enemy. The function is then called during update
+        """
+        raise NotImplementedError("Need to impliment abstract function enemy.do_ai()")
 
     def add_interaction(self, interaction):
         if interaction.tag in [other_interaction.tag for other_interaction in self.interactions]:
@@ -86,3 +56,43 @@ class Enemy(entity.Entity):
                     extended_interaction.reset()
                 return
         self.interactions.append(interaction)
+
+
+class Enemy_Zombie(Enemy):
+    """
+    Class for the zombie
+    """
+
+    def __init__(self, x, y):
+        super(Enemy_Zombie, self).__init__("mummy", x, y, 1, 3)
+        self.x = x
+        self.y = y
+
+        self.FOLLOW_RANGE = 600
+
+    def do_ai(self, dt, player_rect):
+        # If we finished an attack last turn, we want to reset these variables so the hit happens only once
+        if self.deal_damage:
+            self.hurtbox = None
+            self.deal_damage = False
+
+        # If we're in an attack windup
+        if self.attacking:
+            self.attack_timer += (dt * self.attack_speed_percent)
+            if self.attack_timer >= self.ATTACK_SPEED:
+                self.deal_damage = True
+                self.attacking = False
+        else:
+            player_center = util.get_center(player_rect)
+            self_center = self.get_center()
+            player_distance = util.get_distance(self_center, player_center)
+            if self.collides(player_rect):
+                # begin an attack windup
+                self.attacking = True
+                self.hurtbox = player_rect
+                self.attack_timer = 0
+                self.vx, self.vy = (0, 0)
+            elif player_distance <= self.FOLLOW_RANGE:
+                # Vectorize movement relative to player
+                vector_to_player = ((player_center[0] - self_center[0]), (player_center[1] - self_center[1]))
+                self.vx, self.vy = util.scale_vector(vector_to_player, self.MOVE_SPEED)
